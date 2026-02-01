bb09fc2ca859e680ed0ae5b8a0a8cfc6396a96ca
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2026-02-01 10:45:25 +0100

    feat: add backend database models and initial migration

---
+++ backend/src/models/Game.model.js
@@ -0,0 +1,10 @@
+/**
+ * Base model for Game results.
+ */
+const db = require('../config/database');
+
+const GameModel = {
+  // TODO: Implementation logic
+};
+
+module.exports = GameModel;
---
+++ backend/src/models/Transaction.model.js
@@ -0,0 +1,10 @@
+/**
+ * Base model for Wallet Transactions.
+ */
+const db = require('../config/database');
+
+const TransactionModel = {
+  // TODO: Implementation logic
+};
+
+module.exports = TransactionModel;
